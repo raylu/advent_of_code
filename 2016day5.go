0dd44cdfcf6233ca268a03ba381f75fa4935cf17
raylu <lurayl@gmail.com>
2017-09-07 02:14:02 -0700

    allocate less

--- 2016day5.go
+++ 2016day5.go
@@ -48,10 +48,18 @@ func main() {
 	}
 }
 
+const maxInt = int64(^uint64(0) >> 1)
+
 func checkInts(input []byte, testInts chan int64, answers chan AnswerPair) {
+	maxIntLen := len(strconv.FormatInt(maxInt, 10))
+	test := make([]byte, 0, len(input) + maxIntLen)
+	test = append(test, input...)
+	buffer := make([]byte, maxIntLen)
 	for {
 		testInt := <-testInts
-		test := strconv.AppendInt(input, testInt, 10)
+		//test := strconv.AppendInt(buffer, testInt, 10)
+		test = formatBits(test, len(input), buffer, uint64(testInt), testInt < 0)
+		//fmt.Printf("testing %s %d %d\n", test, len(test), cap(test))
 		hash := md5.Sum(test)
 		if hash[0] == 0 && hash[1] == 0 && hash[2] & 0xf0 == 0 {
 			position := hash[2] & 0x0f
@@ -61,4 +69,38 @@ func checkInts(input []byte, testInts chan int64, answers chan AnswerPair) {
 			}
 		}
 	}
+}
+
+
+// mostly copied from strconv/itoa.go
+func formatBits(dst []byte, offset int, buffer []byte, u uint64, neg bool) []byte {
+	i := len(buffer)
+	if neg {
+		u = -u
+	}
+
+	// u guaranteed to fit into a uintptr
+	us := uintptr(u)
+	for us >= 10 {
+		i--
+		q := us / 10
+		buffer[i] = byte(us - q*10 + '0')
+		us = q
+	}
+	// u < 10
+	i--
+	buffer[i] = byte(us + '0')
+	// add sign, if any
+	if neg {
+		i--
+		buffer[i] = '-'
+	}
+
+	intLen := len(buffer) - i
+	dst = dst[0:offset + intLen]
+	start := i
+	for ; i < len(buffer); i++ {
+		dst[offset + i - start] = buffer[i]
+	}
+	return dst
 }
