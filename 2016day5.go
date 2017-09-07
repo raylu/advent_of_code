881fbac6dcb39921c7131deb656f4f040e65a875
raylu <lurayl@gmail.com>
2017-09-07 01:18:04 -0700

    2016 day 5 part 2

--- 2016day5.go
+++ 2016day5.go
@@ -5,42 +5,60 @@ import "crypto/md5"
 import "os"
 import "strconv"
 
+type AnswerPair struct {
+	Position byte
+	OutputChar byte
+}
 
 func main() {
 	input := []byte(os.Args[1])
-	testInts := make(chan int64)
-	const threads = 1
-	answers := make(chan byte, threads)
+	const threads = 4
+	testInts := make(chan int64, threads)
+	answers := make(chan AnswerPair, threads)
 	for i := int64(0); i < threads; i++ {
 		go checkInts(input, testInts, answers)
 	}
 
-	answer := make([]byte, 8)
-	numAnswers := 0
-	loop:
-	for i := int64(0); ; i++ {
-		select {
-		case outputChar := <-answers:
-			answer[numAnswers] = outputChar
-			numAnswers++
-			fmt.Printf("%s\n", answer)
-			if numAnswers == 8 {
-				break loop
+	go func() {
+		answer := make([]byte, 8)
+		for {
+			pair := <-answers
+			if answer[pair.Position] != 0 {
+				continue
 			}
-		default:
+			answer[pair.Position] = pair.OutputChar
+			numAnswers := 0
+			for _, b := range(answer) {
+				if b == 0 {
+					fmt.Print("_")
+				} else {
+					fmt.Printf("%c", b)
+					numAnswers += 1
+				}
+			}
+			fmt.Println()
+			if numAnswers == 8 {
+				os.Exit(0)
+			}
 		}
+	}()
+
+	for i := int64(0); ; i++ {
 		testInts <-i
 	}
 }
 
-func checkInts(input []byte, testInts chan int64, answers chan byte) {
+func checkInts(input []byte, testInts chan int64, answers chan AnswerPair) {
 	for {
 		testInt := <-testInts
 		test := strconv.AppendInt(input, testInt, 10)
 		hash := md5.Sum(test)
 		if hash[0] == 0 && hash[1] == 0 && hash[2] & 0xf0 == 0 {
-			outputChar := strconv.FormatUint(uint64(hash[2] & 0x0f), 16)[0]
-			answers <- outputChar
+			position := hash[2] & 0x0f
+			if position < 8 {
+				outputChar := strconv.FormatUint(uint64(hash[3] & 0xf0), 16)[0]
+				answers <- AnswerPair{position, outputChar}
+			}
 		}
 	}
 }
