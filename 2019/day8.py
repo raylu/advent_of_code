795c26e68fc735507d9146c34ccb50712ed32ee9
raylu <lurayl@gmail.com>
2019-12-07 21:40:58 -0800

    2019 day 8 part 2

--- 2019/day8.py
+++ 2019/day8.py
@@ -10,15 +10,23 @@ def main():
 	width = 25
 	height = 6
 	layer_size = width * height
+	image = [[' '] * width for _ in range(height)]
 
 	min_zeroes = float('inf')
-	for offset in range(0, len(image_data), layer_size):
-		layer = image_data[offset:offset + layer_size]
-		zeroes = layer.count('0')
-		if zeroes < min_zeroes:
-			min_zeroes = zeroes
-			ans = layer.count('1') * layer.count('2')
-	print(ans)
+	for layer_offset in range(0, len(image_data), layer_size):
+		layer = image_data[layer_offset:layer_offset + layer_size]
+		for row, row_offset in enumerate(range(0, layer_size, width)):
+			row_pixels = layer[row_offset:row_offset+width]
+			for col, pixel in enumerate(row_pixels):
+				if pixel != '2' and image[row][col] == ' ':
+					image[row][col] = pixel
+	for row_pixels in image:
+		for pixel in row_pixels:
+			if pixel == '1':
+				print('W', end='')
+			else:
+				print(' ', end='')
+		print()
 
 if __name__ == '__main__':
 	main()
