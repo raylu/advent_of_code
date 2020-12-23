855226277b121d93880cf0d9bc2c6895dbcd28a4
raylu <90059+raylu@users.noreply.github.com>
2020-12-22 16:12:04 -0800

    2020 day 3 part 2

--- 2020/day3.rs
+++ 2020/day3.rs
@@ -3,19 +3,32 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-	let lines = read_lines("day3_input");
+	let lines: Vec<_> = read_lines("day3_input").collect();
+	let nums = [
+		count_trees(&lines, 1, 1),
+		count_trees(&lines, 3, 1),
+		count_trees(&lines, 5, 1),
+		count_trees(&lines, 7, 1),
+		count_trees(&lines, 1, 2),
+	];
+	println!("{:?} = {}", nums, nums.iter().fold(1, |prod: i64, &i| prod * i as i64));
+}
+
+fn count_trees(lines: &Vec<String>, right: usize, down: usize) -> i32 {
 	let mut x_pos = 0;
+	let mut y_pos = 0;
 	let mut trees = 0;
-	for line in lines {
-		let line_bytes = line.as_bytes();
+	while y_pos < lines.len() {
+		let line_bytes = lines[y_pos].as_bytes();
 		let tile = line_bytes[x_pos % line_bytes.len()];
-		println!("{} {}:\t{}", line, x_pos, tile as char);
 		if tile == b'#' {
 			trees += 1;
 		}
-		x_pos += 3;
+
+		x_pos += right;
+		y_pos += down;
 	}
-	println!("{}", trees);
+	trees
 }
 
 fn read_lines(path: &str) -> impl Iterator<Item = String> {
