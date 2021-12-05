64cc79b39636c190790d960733eac7866e312a9f
raylu <90059+raylu@users.noreply.github.com>
2021-12-04 21:47:31 -0800

    2021 day 5 part 2

--- 2021/day5.py
+++ 2021/day5.py
@@ -3,6 +3,12 @@
 import utils
 
 def part1():
+	print(count_overlaps(False))
+
+def part2():
+	print(count_overlaps(True))
+
+def count_overlaps(diagonals: bool):
 	grid = [[0] * 1000 for _ in range(1000)]
 	for line in utils.iter_lines(5):
 		start, end = line.split(' -> ')
@@ -16,11 +22,19 @@ def part1():
 			y = y1
 			for x in range(min(x1, x2), max(x1, x2)+1):
 				grid[x][y] += 1
+		elif diagonals:
+			dx = dy = 1
+			if x1 > x2:
+				dx = -1
+			if y1 > y2:
+				dy = -1
+			for x, y in zip(range(x1, x2+dx, dx), range(y1, y2+dy, dy)):
+				grid[x][y] += 1
 
 	overlaps = 0
 	for row in grid:
 		overlaps += sum(1 for cell in row if cell >= 2)
-	print(overlaps)
+	return overlaps
 
 if __name__ == '__main__':
-	part1()
+	part2()
