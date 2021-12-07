c3c33f64e819c4513db4d226d1e1315265e47d6c
raylu <90059+raylu@users.noreply.github.com>
2021-12-06 22:40:37 -0800

    2021 day 6 part 2

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
+import collections
 
 def part1():
-	with open('day6_input', 'r', encoding='ascii') as f:
-		state = list(map(int, f.read().rstrip().split(',')))
-
+	state = read_state()
 	for _ in range(80):
 		new_state = []
 		for i, fish in enumerate(state):
@@ -16,5 +15,26 @@ def part1():
 		state = new_state
 	print(len(state))
 
+def part2():
+	state = read_state()
+	fish_counts = collections.Counter(state)
+	for _ in range(256):
+		new_counts = {}
+		new_sixes = 0
+		for stage, num in fish_counts.items():
+			if stage == 0:
+				new_counts[8] = num
+				new_sixes = num
+			else:
+				new_counts[stage - 1] = num
+		new_counts.setdefault(6, 0)
+		new_counts[6] += new_sixes
+		fish_counts = new_counts
+	print(sum(new_counts.values()))
+
+def read_state():
+	with open('day6_input', 'r', encoding='ascii') as f:
+		return list(map(int, f.read().rstrip().split(',')))
+
 if __name__ == '__main__':
-	part1()
+	part2()
