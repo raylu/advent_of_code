0d80470c301e8f139e7acabe6e01720e42859355
raylu <90059+raylu@users.noreply.github.com>
2021-12-06 22:54:58 -0800

    2021 day 7 part 2

--- 2021/day7.py
+++ 2021/day7.py
@@ -15,5 +15,19 @@ def part1():
 			print('new min', fuel)
 			min_cost = fuel
 
+def part2():
+	positions = utils.read_int_line(7)
+	max_pos = max(positions)
+	min_cost = float('inf')
+	for i in range(1, max_pos):
+		fuel = 0
+		for pos in positions:
+			distance = abs(pos - i)
+			fuel += distance * (distance + 1) // 2
+
+		if fuel < min_cost:
+			print('new min', fuel)
+			min_cost = fuel
+
 if __name__ == '__main__':
-	part1()
+	part2()
