e137857c3bbed6e73554c38209532d211d16dd00
raylu <90059+raylu@users.noreply.github.com>
2021-12-08 21:49:10 -0800

    2021 day 9 part 2

--- 2021/day9.py
+++ 2021/day9.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python3
 
+import functools
 import operator
 
 import utils
 
+cardinal_directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
 def part1():
 	grid = [list(map(int, line)) for line in utils.iter_lines(9)]
 	risk_level = 0
-	for i, row in enumerate(grid):
-		for j, c in enumerate(row):
-			for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-				if 0 <= i + di < len(grid) and 0 <= j + dj < len(row) and \
-						grid[i+di][j+dj] <= c:
-					break
-			else: # low point
-				risk_level += c + 1
+	for i, j in iter_basins(grid):
+		risk_level += grid[i][j] + 1
 	print(risk_level)
 
+def part2():
+	grid = [list(map(int, line)) for line in utils.iter_lines(9)]
+	basin_sizes = []
+	for i, j in iter_basins(grid):
+		seen = set()
+		find_basin(grid, i, j, seen)
+		basin_sizes.append(len(seen))
+
+	basin_sizes.sort()
+	print(basin_sizes)
+	print(functools.reduce(operator.mul, basin_sizes[-3:], 1))
+
+def iter_basins(grid):
+	for i, row in enumerate(grid):
+		for j, c in enumerate(row):
+			for di, dj in cardinal_directions:
+				if in_bounds(grid, i + di, j + dj) and grid[i+di][j+dj] <= c:
+					break
+			else: # low point
+				yield i, j
+
+def in_bounds(grid, i, j):
+	return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+def find_basin(grid, i, j, seen):
+	for di, dj in cardinal_directions:
+		new_coords = i + di, j + dj
+		if new_coords not in seen and in_bounds(grid, *new_coords) and grid[i+di][j+dj] != 9:
+			seen.add(new_coords)
+			find_basin(grid, *new_coords, seen)
+
 if __name__ == '__main__':
-	part1()
+	part2()
