dc8f7599899a97ba847ddfca58dfcd6d6ffacea7
raylu <90059+raylu@users.noreply.github.com>
2021-12-11 22:02:47 -0800

    2021 day 12 part 2

--- 2021/day12.py
+++ 2021/day12.py
@@ -5,16 +5,22 @@ import collections
 import utils
 
 def part1():
+	graph = read_graph()
+	print(paths_1(graph, set(['start']), 'start'))
+
+def part2():
+	graph = read_graph()
+	print(paths_2(graph, set(['start']), 'start', None))
+
+def read_graph():
 	graph = collections.defaultdict(list)
 	for line in utils.iter_lines(12):
 		node1, node2 = line.split('-')
 		graph[node1].append(node2)
 		graph[node2].append(node1)
+	return graph
 
-	smalls = set(['start'])
-	print(paths(graph, smalls, 'start'))
-
-def paths(graph, smalls, node):
+def paths_1(graph, smalls, node):
 	subpaths = 0
 	for dest in graph[node]:
 		if dest == 'end':
@@ -25,10 +31,31 @@ def paths(graph, smalls, node):
 		if is_small:
 			if dest in smalls:
 				continue
-			subpaths += paths(graph, smalls | {dest}, dest)
+			subpaths += paths_1(graph, smalls | {dest}, dest)
 		else:
-			subpaths += paths(graph, smalls, dest)
+			subpaths += paths_1(graph, smalls, dest)
 	return subpaths
 
+def paths_2(graph, smalls, node, twice):
+	subpaths = 0
+	for dest in graph[node]:
+		if dest == 'end':
+			subpaths += 1
+			continue
+		elif dest == 'start':
+			continue
+
+		is_small = dest == dest.lower()
+		if is_small:
+			if dest in smalls:
+				if twice is None:
+					subpaths += paths_2(graph, smalls, dest, dest)
+			else:
+				subpaths += paths_2(graph, smalls | {dest}, dest, twice)
+		else:
+			subpaths += paths_2(graph, smalls, dest, twice)
+	return subpaths
+
+
 if __name__ == '__main__':
-	part1()
+	part2()
