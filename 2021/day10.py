7c706226825209a4680509cd7e9bae2861710ae8
raylu <90059+raylu@users.noreply.github.com>
2021-12-11 22:18:06 -0800

    2021 day 10 part 2

--- 2021/day10.py
+++ 2021/day10.py
@@ -16,12 +16,32 @@ def part1():
 	}
 	score = 0
 	for line in utils.iter_lines(10):
-		illegal = find_illegal(line)
+		illegal, _ = illegal_or_complete(line)
 		if illegal is not None:
 			score += points[illegal]
 	print(score)
 
-def find_illegal(line):
+def part2():
+	points = {
+		')': 1,
+		']': 2,
+		'}': 3,
+		'>': 4,
+	}
+	scores = []
+	for line in utils.iter_lines(10):
+		_, stack = illegal_or_complete(line)
+		if stack is None:
+			continue
+		score = 0
+		while stack:
+			c = stack.pop()
+			score = score * 5 + points[c]
+		scores.append(score)
+	scores.sort()
+	print(scores[len(scores) // 2])
+
+def illegal_or_complete(line):
 	stack = []
 	for c in line:
 		if c == '(':
@@ -33,7 +53,8 @@ def find_illegal(line):
 		elif c == '<':
 			stack.append('>')
 		elif stack.pop() != c:
-			return c
+			return c, None
+	return None, stack
 
 if __name__ == '__main__':
-	part1()
+	part2()
