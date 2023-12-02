bc603fc022a49bf10244cbdd92b391fb4dd593f6
raylu <90059+raylu@users.noreply.github.com>
2023-12-01 16:36:30 -0800

    2023 day 1 part 2

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
 
-import typing
-
 def main():
 	with open('day1_input', 'r') as f:
 		lines = f.read().splitlines()
 
 	total = 0
 	for line in lines:
-		total += int(first_num(line) + first_num(reversed(line)))
+		total += int(first_num(line) + last_num(line))
 	print(total)
 
-def first_num(line: typing.Sequence[str]) -> str:
-	for c in line:
+DIGITS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+def first_num(line: str) -> str:
+	for i, c in enumerate(line):
 		if c.isdigit():
 			return c
+		for j, digit in enumerate(DIGITS):
+			if line[i:].startswith(digit):
+				return str(j+1)
+	raise AssertionError
+
+def last_num(line: str) -> str:
+	for i in range(len(line) - 1, -1, -1):
+		if line[i].isdigit():
+			return line[i]
+		for j, digit in enumerate(DIGITS):
+			if line[i:].startswith(digit):
+				return str(j+1)
+	raise AssertionError
 
 if __name__ == '__main__':
 	main()
