2f04a89f09c963b528e101af4d431494dca62cbf
raylu <90059+raylu@users.noreply.github.com>
2024-12-01 23:07:46 -0800

    rustfmt config

--- 2024/src/bin/day1.rs
+++ 2024/src/bin/day1.rs
@@ -3,27 +3,27 @@ use std::collections::HashMap;
 use aoc2024::read_lines;
 
 fn main() {
-    let mut l1: Vec<i32> = vec![];
-    let mut l2: HashMap<i32, i32> = HashMap::with_capacity(1000);
-    for line in read_lines("day1.txt") {
-        let (left, right) = line.split_once("   ").unwrap();
-        l1.push(left.parse::<i32>().unwrap());
-        let right = right.parse::<i32>().unwrap();
-        match l2.get_mut(&right) {
-            Some(freq) => {
-                *freq += 1;
-            }
-            None => {
-                l2.insert(right, 1);
-            }
-        }
-    }
-    let similarity = l1
-        .into_iter()
-        .map(|left| {
-            let freq = l2.get(&left).map_or(0, |freq| *freq);
-            left * freq
-        })
-        .sum::<i32>();
-    println!("similarity: {}", similarity);
+	let mut l1: Vec<i32> = vec![];
+	let mut l2: HashMap<i32, i32> = HashMap::with_capacity(1000);
+	for line in read_lines("day1.txt") {
+		let (left, right) = line.split_once("   ").unwrap();
+		l1.push(left.parse::<i32>().unwrap());
+		let right = right.parse::<i32>().unwrap();
+		match l2.get_mut(&right) {
+			Some(freq) => {
+				*freq += 1;
+			},
+			None => {
+				l2.insert(right, 1);
+			},
+		}
+	}
+	let similarity = l1
+		.into_iter()
+		.map(|left| {
+			let freq = l2.get(&left).map_or(0, |freq| *freq);
+			left * freq
+		})
+		.sum::<i32>();
+	println!("similarity: {}", similarity);
 }
--- 2024/src/bin/day2.rs
+++ 2024/src/bin/day2.rs
@@ -1,69 +1,65 @@
 use aoc2024::read_lines;
 
 fn main() {
-    let safe = read_lines("day2.txt")
-        .map(|line| {
-            let report: Vec<i32> = line.split(' ').map(|x| x.parse::<i32>().unwrap()).collect();
-            if is_safe(&report) {
-                1
-            } else {
-                for i in 0..report.len() {
-                    let dampened = report[0..i]
-                        .iter()
-                        .cloned()
-                        .chain(report[i + 1..].iter().cloned())
-                        .collect();
-                    if is_safe(&dampened) {
-                        return 1;
-                    }
-                }
-                0
-            }
-        })
-        .sum::<i32>();
-    println!("safe: {}", safe);
+	let safe = read_lines("day2.txt")
+		.map(|line| {
+			let report: Vec<i32> = line.split(' ').map(|x| x.parse::<i32>().unwrap()).collect();
+			if is_safe(&report) {
+				1
+			} else {
+				for i in 0..report.len() {
+					let dampened = report[0..i].iter().cloned().chain(report[i + 1..].iter().cloned()).collect();
+					if is_safe(&dampened) {
+						return 1;
+					}
+				}
+				0
+			}
+		})
+		.sum::<i32>();
+	println!("safe: {}", safe);
 }
 
 fn is_safe(report: &Vec<i32>) -> bool {
-    // adjacent levels >= 1 and <= 3
-    if !AdjacentPairs::new(report).all(|(prev, cur)| {
-        let diff = cur.abs_diff(prev);
-        diff >= 1 && diff <= 3
-    }) {
-        return false;
-    }
-    // all increasing/decreasing
-    return AdjacentPairs::new(report).all(|(prev, cur)| cur > prev)
-        || AdjacentPairs::new(report).all(|(prev, cur)| cur < prev);
+	// adjacent levels >= 1 and <= 3
+	if !AdjacentPairs::new(report).all(|(prev, cur)| {
+		let diff = cur.abs_diff(prev);
+		diff >= 1 && diff <= 3
+	}) {
+		return false;
+	}
+	// all increasing/decreasing
+	return AdjacentPairs::new(report).all(|(prev, cur)| cur > prev)
+		|| AdjacentPairs::new(report).all(|(prev, cur)| cur < prev);
 }
 
 struct AdjacentPairs<'a, T> {
-    vector: &'a Vec<T>,
-    index: usize,
+	vector: &'a Vec<T>,
+	index: usize,
 }
 impl<'a, T> AdjacentPairs<'a, T> {
-    fn new(vector: &'a Vec<T>) -> Self {
-        Self { vector, index: 1 }
-    }
+	fn new(vector: &'a Vec<T>) -> Self {
+		Self { vector, index: 1 }
+	}
 }
 impl<'a, T> Iterator for AdjacentPairs<'a, T>
 where
-    T: Copy,
+	T: Copy,
 {
-    type Item = (T, T);
+	type Item = (T, T);
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index == self.vector.len() {
-            None
-        } else {
-            let pair = Some((self.vector[self.index - 1], self.vector[self.index]));
-            self.index += 1;
-            pair
-        }
-    }
+	fn next(&mut self) -> Option<Self::Item> {
+		if self.index == self.vector.len() {
+			None
+		} else {
+			let pair = Some((self.vector[self.index - 1], self.vector[self.index]));
+			self.index += 1;
+			pair
+		}
+	}
 
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        let remaining = self.vector.len() - self.index;
-        (remaining, Some(remaining))
-    }
+	fn size_hint(&self) -> (usize, Option<usize>) {
+		let remaining = self.vector.len() - self.index;
+		(remaining, Some(remaining))
+	}
 }
