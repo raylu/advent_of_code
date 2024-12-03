99555daa43a2a73920e512e0ea8bca8fbdf89047
raylu <90059+raylu@users.noreply.github.com>
2024-12-02 23:51:18 -0800

    day 3 part 2

--- 2024/src/bin/day3.rs
+++ 2024/src/bin/day3.rs
@@ -1,26 +1,60 @@
 use aoc2024::read_file;
 
 fn main() {
-	let instr = read_file("day3.txt");
-	let mut instr = instr.as_str();
+	let instructions = read_file("day3.txt");
+	let mut instructions = instructions.as_str();
+	let mut mul_enabled = true;
 	let mut sum = 0;
-	while let Some((index, product)) = parse(instr) {
-		sum += product.unwrap_or_default();
-		instr = &instr[index..];
+	while let Some((index, instr)) = parse(instructions) {
+		match instr {
+			Some(Instr::Mul(product)) => {
+				if mul_enabled {
+					sum += product;
+				}
+			},
+			Some(Instr::Do) => mul_enabled = true,
+			Some(Instr::Dont) => mul_enabled = false,
+			None => {},
+		}
+		instructions = &instructions[index..];
 	}
 	println!("{}", sum);
 }
 
-fn parse(instr: &str) -> Option<(usize, Option<i32>)> {
+enum Instr {
+	Mul(i32),
+	Do,
+	Dont,
+}
+
+fn parse(instr: &str) -> Option<(usize, Option<Instr>)> {
 	print!("{}", instr);
-	let start = instr.find("mul(")?;
-	let instr = &instr[start + 4..];
-	let end = instr.find(')')?;
-	if let Some((op1, op2)) = instr[..end].split_once(',') {
-		if let Ok(product) = op1.parse::<i32>().and_then(|op1| op2.parse::<i32>().map(|op2| op1 * op2)) {
-			println!("\t{:?}", product);
-			return Some((start + 4 + end + 1, Some(product)));
+	let mul_start = instr.find("mul(");
+	let do_start = instr.find("do()");
+	let dont_start = instr.find("don't()");
+	let min = mul_start
+		.unwrap_or(usize::MAX)
+		.min(do_start.unwrap_or(usize::MAX))
+		.min(dont_start.unwrap_or(usize::MAX));
+
+	if mul_start.is_some() && min == mul_start.unwrap() {
+		let start = mul_start.unwrap();
+		let instr = &instr[start + 4..];
+		let end = instr.find(')')?;
+		if let Some((op1, op2)) = instr[..end].split_once(',') {
+			if let Ok(product) = op1.parse::<i32>().and_then(|op1| op2.parse::<i32>().map(|op2| op1 * op2)) {
+				println!("\t{:?}", product);
+				return Some((start + 4 + end + 1, Some(Instr::Mul(product))));
+			}
 		}
+		Some((start + 4, None))
+	} else if do_start.is_some() && min == do_start.unwrap() {
+		let start = do_start.unwrap();
+		Some((start + 4, Some(Instr::Do)))
+	} else if dont_start.is_some() && min == dont_start.unwrap() {
+		let start = dont_start.unwrap();
+		Some((start + 7, Some(Instr::Dont)))
+	} else {
+		None
 	}
-	Some((start + 4, None))
 }
