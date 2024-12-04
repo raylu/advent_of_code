4ef7d89ac3f38a59d3d76a3bb55230dd03728577
raylu <90059+raylu@users.noreply.github.com>
2024-12-03 22:08:02 -0800

    day 4 part 2

--- 2024/src/bin/day4.rs
+++ 2024/src/bin/day4.rs
@@ -2,31 +2,36 @@ use aoc2024::read_lines;
 
 fn main() {
 	let word_search: Vec<Vec<u8>> = read_lines("day4.txt").map(|line| line.as_bytes().to_vec()).collect();
-	let dirs: [(i8, i8); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
 	let mut xmas = 0;
 	for (y, row) in word_search.iter().enumerate() {
 		for (x, ch) in row.iter().enumerate() {
-			if *ch == b'X' {
-				xmas += dirs
-					.iter()
-					.filter(|dir| is_xmas(&word_search, y.try_into().unwrap(), x.try_into().unwrap(), **dir))
-					.count();
+			if *ch == b'A' && is_xmas(&word_search, y.try_into().unwrap(), x.try_into().unwrap()) {
+				println!("at {}, {}", y, x);
+				xmas += 1;
 			}
 		}
 	}
 	println!("xmas: {}", xmas);
 }
 
-fn is_xmas(word_search: &Vec<Vec<u8>>, y: i32, x: i32, dir: (i8, i8)) -> bool {
-	let mut word: [u8; 4] = [0; 4];
-	for i in 0..4 {
-		let y: i32 = y + (i32::try_from(i).unwrap() * i32::from(dir.0));
-		let x: i32 = x + (i32::try_from(i).unwrap() * i32::from(dir.1));
+fn is_xmas(word_search: &[Vec<u8>], y: i32, x: i32) -> bool {
+	let delta1: [(i8, i8); 2] = [(-1, -1), (1, 1)];
+	let delta2: [(i8, i8); 2] = [(-1, 1), (1, -1)];
+	let word1 = get_word(word_search, y, x, &delta1);
+	let word2 = get_word(word_search, y, x, &delta2);
+	(word1 == [b'M', b'S'] || word1 == [b'S', b'M']) && (word2 == [b'M', b'S'] || word2 == [b'S', b'M'])
+}
+
+fn get_word(word_search: &[Vec<u8>], y: i32, x: i32, deltas: &[(i8, i8)]) -> [u8; 2] {
+	let mut word: [u8; 2] = [0; 2];
+	for (i, (dy, dx)) in deltas.iter().enumerate() {
+		let y: i32 = y + i32::from(*dy);
+		let x: i32 = x + i32::from(*dx);
 		if y < 0 || y >= word_search.len().try_into().unwrap() || x < 0 || x >= word_search[0].len().try_into().unwrap()
 		{
-			return false;
+			break;
 		}
 		word[i] = word_search[usize::try_from(y).unwrap()][usize::try_from(x).unwrap()];
 	}
-	word == [b'X', b'M', b'A', b'S']
+	word
 }
